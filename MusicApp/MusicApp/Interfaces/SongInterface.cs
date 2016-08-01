54b965841800190c085abbb52a0cfae1af6e9eb2
brandon berning <brandon berning>
2016-08-01 10:33:03 -0600

    added song model and song interface

---
+++ MusicApp/MusicApp/Interfaces/SongInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApp.Interfaces
+{
+    interface SongInterface
+    {
+        void addSong(Song song);
+        void deleteSong(int id);
+        void editSong(Song s);
+    }
+}
---
+++ MusicApp/MusicApp/Models/Song.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicApp.Models
+{
+    public class Song
+    {
+        public string Title { get; set; }
+        public string Filename { get; set; }
+        public string Artst { get; set; }
+        public string Album { get; set; }
+        public double length { get; set; }
+
+    }
+}
