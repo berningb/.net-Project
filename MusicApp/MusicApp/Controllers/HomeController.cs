47dab141b59eddc72a561345f1f8875705cf39db
Kurtis Lloyd <klloyd@student.neumont.edu>
2016-07-31 23:37:53 -0600

    Added basic query from local Neo4j Database and displayed CRUD operiations on index page. This will not work on other machines until the database is online.

--- MusicApp/MusicApp/Controllers/HomeController.cs
+++ MusicApp/MusicApp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Neo4j.Driver.V1;
 
 namespace MusicApp.Controllers
 {
@@ -10,7 +11,28 @@ namespace MusicApp.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.List = NeoQuery();
+
+
             return View();
+        }
+        public List<string> NeoQuery()
+        {
+            using (var driver = GraphDatabase.Driver("bolt://localhost", AuthTokens.Basic("neo4j", "test")))
+            using (var session = driver.Session())
+            {
+                List<string> ListOutput = new List<string>();
+                string output;
+               // session.Run("CREATE (a:User {name:'Kurtis'}), (b:User{name:'Melissa'})");
+                var result = session.Run("MATCH (n:User) RETURN n.name as name");
+                foreach (var record in result)
+                {
+                    output = ($"{ record["name"].As<string>()}");
+                    ListOutput.Add(output);
+                }
+                return ListOutput;
+            }
+
         }
     }
 }
