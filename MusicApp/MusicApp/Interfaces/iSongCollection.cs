39783fc20d07e5039abb164b33c1dffe1eae199f
Efren <moker49@gmail.com>
2016-08-02 11:18:35 -0600

    Created interfaces for the models and implemented them

---
+++ MusicApp/MusicApp/Interfaces/iSongCollection.cs
@@ -0,0 +1,14 @@
+﻿using MusicApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicApp.Interfaces
+{
+    public interface iSongCollection
+    {
+        void Add(Song song);
+        void Add(iSongCollection collection);
+    }
+}
--- MusicApp/MusicApp/Models/Album.cs
+++ MusicApp/MusicApp/Models/Album.cs
@@ -1,14 +1,30 @@
-﻿using System;
+﻿using MusicApp.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace MusicApp.Models
 {
-    public class Album
+    public class Album : iLikeable, iSongCollection
     {
         public string Name { get; set; }
         public Artist Owner { get; set; }
-        public List<Song> songs { get; set; }
+        public List<Song> Songs { get; set; }
+
+        public void Add(iSongCollection collection)
+        {
+            //if artist == owner, add each item in collection to Songs
+        }
+
+        public void Add(Song song)
+        {
+            //if artist == owner, add song to Songs
+        }
+
+        public void Like(Artist artist)
+        {
+            artist.Likes.Add(this);
+        }
     }
 }
