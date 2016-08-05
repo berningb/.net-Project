bc23725bdc33070fcb42d11029774323de0f9c26
Kurtis Lloyd <klloyd@student.neumont.edu>
2016-08-05 10:48:53 -0600

    Fixed error

--- MusicApp/Neo4j/NeoMain.cs
+++ MusicApp/Neo4j/NeoMain.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Neo4j.Driver.V1;
-using Neo4j.Interfaces;
 
 namespace Neo4j
 {
-    public static class NeoMain
+    public class NeoMain
     {
-        public static List<string> NeoQuery()
+        public List<string> NeoQuery()
         {
             using (var driver = GraphDatabase.Driver("bolt://localhost", AuthTokens.Basic("neo4j", "test")))
             using (var session = driver.Session())
@@ -29,7 +28,7 @@ namespace Neo4j
 
         }
 
-        public static void CreateArtist(Artist artist)
+        public void CreateArtist(Artist artist)
         {
             using (var driver = GraphDatabase.Driver("bolt://localhost", AuthTokens.Basic("neo4j", "test")))
             using (var session = driver.Session())
@@ -37,30 +36,13 @@ namespace Neo4j
                 session.Run("CREATE (a:Artist {name:" +  artist.Name + "}) SET a.Email = " + artist.Email );
             }
         }
-        public static void Like(Artist artist, ILikeable likedThing)
-        {
-            // likedThing -Likee-> artist
-            // artist -Likes-> likedThing
-        }
-        public static void CreateSong(Song song)
-        {
-            using (var driver = GraphDatabase.Driver("bolt://localhost", AuthTokens.Basic("neo4j", "test")))
-            using (var session = driver.Session())
-            {
-                session.Run("CREATE (a:Song {title:" + song.Title + "}, ");
-            }
-            // song -owner-> song.Owner
-            // song.Owner -Songs-> song
-        }
-        public static void CreatePlaylist(Playlist playlist)
+        public void CreatePlaylist(Playlist playlist, List<Song> songs)
         {
             using (var driver = GraphDatabase.Driver("bolt://localhost", AuthTokens.Basic("neo4j", "test")))
                 using(var session = driver.Session())
             {
                 session.Run("CREATE (a:Playlist {Title:" + playlist.Title + "} ");
             }
-            // palylist -owner-> playlist.Owner
-            // playlist.Owner -Playlists-> playlist
         }
         public void CreateSong(Song song, Artist artist)
         {
@@ -68,17 +50,43 @@ namespace Neo4j
                 using(var session = driver.Session())
             {
                 session.Run("CREATE (a:Song {Title:" + song.Title + "}) SET a.Filename = " + song.Filename + ", a.length" + song.length);
-                session.Run("MATCH (b:Artist {name: " + artist.Name + "}), (c:Song {Title: " + song.Title + "}) CREATE (b)-[:OWN]-(c)");
+                session.Run("MATCH (b:Artist {name: " + artist.Name + "}), (c:Song {Title: " + song.Title + "}) CREATE (b)-[:OWN]->(c)");
             }
 
         }
-        public static void AddSongToCollection(ISongCollection collection, Song song, Artist currentUser)
+        public void AddFriend(Artist fromArtist, Artist toArtist)
         {
-            bool authorized = (collection is Album && collection.Owner != currentUser) ? false : true;
-            if (authorized)
+            using (var driver = GraphDatabase.Driver("bolt://localhost", AuthTokens.Basic("neo4j", "test")))
+                using(var session = driver.Session())
             {
-                // do it
+                session.Run("MATCH(b:Artist {name: " + fromArtist.Name + "}), (c:Artist {name: " + toArtist.Name + "}) CREATE (b)-[:FRIEND]->(c)");
             }
         }
+        public void FollowArtist(Artist follower, Artist followee)
+        {
+            using (var driver = GraphDatabase.Driver("bolt://localhost", AuthTokens.Basic("neo4j", "test")))
+            using (var session = driver.Session())
+            {
+                session.Run("MATCH(b:Artist {name: " + follower.Name + "}), (c:Artist {name: " + followee.Name + "}) CREATE (b)-[:FOLLOWS]->(c)");
+            }
+        }
+        public List<Artist> getFollowers(Artist artist)
+        {
+            List<string> outputFollowers = new List<string>();
+            List<Artist> artists = new List<Artist>();
+
+            using (var driver = GraphDatabase.Driver("bolt://localhost", AuthTokens.Basic("neo4j", "test")))
+            using (var session = driver.Session())
+            {
+                var result = session.Run("MATCH (a:Artist")
+            }
+
+                return artists;
+        }
+        public void Album()
+        {
+
+        }
+
     }
 }
