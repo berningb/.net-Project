e5e44b2b37988df702ae17496cfbfc8d4dd3b7f1
Daniel <dkuhrmeyer@gmail.com>
2016-08-11 09:52:56 -0600

    setup basic wavesurfer javascript to get a visual of what it looks like

--- MusicApp/MusicApp/Scripts/wavesurfer.js
+++ MusicApp/MusicApp/Scripts/wavesurfer.js
@@ -1,1 +1,22 @@
-﻿
+﻿var wavesurfer;
+
+window.onload = function () {
+    wavesurfer = WaveSurfer.create({
+        container: '#waveform',
+        waveColor: 'violet',
+        progressColor: 'purple',
+        height: 300,
+        barWidth: 1,
+        containerWidth: 1000
+    });
+
+    wavesurfer.load('../Content/MP3/Weekend.mp3');
+
+    wavesurfer.on('ready', function () {
+        wavesurfer.play();
+        wavesurfer.skipLength = 10;
+        wavesurfer.audioRate = 1;
+    });
+
+
+}
