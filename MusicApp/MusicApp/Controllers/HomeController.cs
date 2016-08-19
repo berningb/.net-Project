3eb8e4406a3688e23992e814bd0623e662ac6265
brandon <berningbrandon9@gmail.com>
2016-08-19 10:32:15 -0600

    added wall and chart page

--- MusicApp/MusicApp/Controllers/HomeController.cs
+++ MusicApp/MusicApp/Controllers/HomeController.cs
@@ -17,22 +17,27 @@ namespace MusicApp.Controllers
     {
         string artistName = web.HttpContext.Current.User.Identity.Name;
         CloudStorageAccount blobAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("BlobConnectionString"));
-        
 
+        String artistinput = "";
         NeoMain neo = new NeoMain();
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult Wall()
-        {
-            return View();
-        }
         public ActionResult Upload()
         {
             return View();
         }
+        public ActionResult Follow()
+        {
+            Artist curr = neo.getArtist(artistName);
+            Artist other = neo.getArtist(artistinput);
+            neo.FollowArtist(curr, other);
+            return View("ProfilePage", other);
+
+        }
+
 
         [HttpGet]
         public ActionResult EditProfile()
@@ -94,6 +99,7 @@ namespace MusicApp.Controllers
 
             ViewBag.username = artistName;
             ViewBag.show = MyJsonConverter.Serialize(MainArty);
+
             return View(MainArty);
         }
 
@@ -140,6 +146,7 @@ namespace MusicApp.Controllers
                     }
                 }
                 Artist finalArtist = new Artist(arty.Name, arty.Email, songs, neo.getFriends(arty), neo.getFollowers(arty));
+                artistinput = finalArtist.Name;
                 return View("ProfilePage", finalArtist);
             }
             return View("Index");
